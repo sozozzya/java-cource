3aa230c6d029a510a11318827f9c4a092780d50d
Sozonov_Ilushka <113029719+sozozzya@users.noreply.github.com>
2025-11-10 23:29:38 +0300

    feat(task-4/1): improve functionality of program for electronic hotel administration

---
+++ task-4/1/src/main/java/ru/senla/task4/hotel/model/StayRecord.java
@@ -0,0 +1,20 @@
+package ru.senla.task4.hotel.model;
+
+import java.time.LocalDate;
+
+public class StayRecord {
+    private final String guestName;
+    private final LocalDate checkIn;
+    private final LocalDate checkOut;
+
+    public StayRecord(String guestName, LocalDate checkIn, LocalDate checkOut) {
+        this.guestName = guestName;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return guestName + " | Check-in: " + checkIn + " | Check-out: " + checkOut;
+    }
+}
